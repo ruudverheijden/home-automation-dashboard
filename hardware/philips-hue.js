e55a2b015e1cf4c68de6d98c81d517a71a6a8520
Ruud Verheijden <ruud.verheijden@gmail.com>
2018-05-12 15:51:02 +0200

    Added route for retrieving a single light object and a route for setting a light to a specific state

--- hardware/philips-hue.js
+++ hardware/philips-hue.js
@@ -1,9 +1,12 @@
-const HueApi = require("node-hue-api").HueApi;
+const hue = require("node-hue-api");
+const HueApi = hue.HueApi;
+const debug = require('debug')('home-automation-dashboard:hardware');
 const config = require('../config/devices.json');
+
+const api = new HueApi(config.philipsHue.host, config.philipsHue.username);
 
 class PhilipsHue {
     constructor () {
-        const api = new HueApi(config.philipsHue.host, config.philipsHue.username);
         this.lights = [];
 
         // Retrieve a list of all lights from the Hue Bridge
@@ -24,11 +27,60 @@ class PhilipsHue {
                     model: light.modelid              
                 });
             });
+
+            debug('Lights retrieved from Philips Hue Bridge');
+        }).fail(error => {
+            debug('Failed to retrieved lights from Philips Hue Bridge due to: ' + error);
         });
     }
 
+    /**
+     * Return a array with objects of all lights
+     */
     getLights () {
         return this.lights;
+    }
+
+    /**
+     * Return the object of a specific light
+     * 
+     * @param {number} id : Identifier of the light
+     * @returns {object || boolean} : Return the light object of false if id was not found
+     */
+    getLight (id) {
+        let output = false;
+
+        this.lights.forEach(light => {
+            if (id == light.id) {
+                output = light;
+            }
+        });
+
+        return output;
+    }
+
+    /**
+     * Set a state for a specific light
+     * @param {number} id : Identifier of the light
+     * @param {object} state : Set the light state
+     * @property {number} state.brightness : Value between 0 and 255
+     * @property {number} state.hue : Value between 0 and 65535
+     * @property {number} state.saturation : Value between 0 and 255
+     * @property {string} state.effect : Can be either 'none' or 'colorloop'
+     * @property {number} state.colorTemperature : Value between 153 and 500
+     */
+    setLight (id, state) {
+        let lightState = hue.lightState.create();
+
+        // Chain the correct state options
+        if (typeof state.on !== 'undefined') { lightState = lightState.on((state.on == 'true')); } // Convert 'on' value to boolean according to Hue library requirement
+        if (typeof state.brightness !== 'undefined') { lightState = lightState.bri(state.brightness); }
+        if (typeof state.hue !== 'undefined') { lightState = lightState.hue(state.hue); }
+        if (typeof state.saturation !== 'undefined') { lightState = lightState.sat(state.saturation); }
+        if (typeof state.effect !== 'undefined') { lightState = lightState.effect(state.effect); }
+        if (typeof state.colorTemperature !== 'undefined') { lightState = lightState.ct(state.colorTemperature); }
+
+        return api.setLightState(id, lightState);
     }
 }
 
--- routes/lights.js
+++ routes/lights.js
@@ -3,7 +3,41 @@ var router = express.Router();
 
 // Get a list of all lights
 router.get('/', function(req, res, next) {
-  res.json(req.app.get('hardware').philipsHue.getLights());
+  let response = req.app.get('hardware').philipsHue.getLights();
+  res.json(response);
+});
+
+/**
+ * Get a specific light
+ */
+router.get('/:id/', function(req, res, next) {
+  let response = req.app.get('hardware').philipsHue.getLight(req.params.id);
+
+  if (!response) {
+    res.status(400).json();
+  } else {
+    res.json(response);
+  }
+});
+
+/**
+ * Set a specific light
+ */
+router.get('/:id/set', function(req, res, next) {
+  const state = {
+    on: req.query.on,
+    brightness: req.query.brightness,
+    hue: req.query.hue,
+    saturation: req.query.saturation,
+    effect: req.query.effect,
+    colorTemperature: req.query.colorTemperature
+  }
+  
+  req.app.get('hardware').philipsHue.setLight(req.params.id, state).then(() => {
+    res.json();
+  }).fail(error => {
+    res.status(500).json({message: error.message});
+  });
 });
 
 module.exports = router;
